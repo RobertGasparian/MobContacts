4aa0cc6a8d9f8e2e9dfae2f0c7c60be85081e14f
RobertGasparian <rob.gasparian@gmail.com>
2017-07-26 11:48:31 +0400

    setting up classes

---
+++ app/src/main/java/com/example/user/mobcontacts/adapters/ContactAdapter.java
@@ -0,0 +1,8 @@
+package com.example.user.mobcontacts.adapters;
+
+/**
+ * Created by User on 7/26/2017.
+ */
+
+public class ContactAdapter {
+}
---
+++ app/src/main/java/com/example/user/mobcontacts/fragments/ContactsFragment.java
@@ -0,0 +1,8 @@
+package com.example.user.mobcontacts.fragments;
+
+/**
+ * Created by User on 7/26/2017.
+ */
+
+public class ContactsFragment {
+}
