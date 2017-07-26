509210576875a0dacb00aa31e2e6af43911d9e8f
RobertGasparian <rob.gasparian@gmail.com>
2017-07-26 11:34:56 +0400

    Initial commit

---
+++ app/src/main/java/com/example/user/mobcontacts/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.mobcontacts.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.user.mobcontacts.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
